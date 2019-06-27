f70ca411c6e21cc0886731cd69be85532e3e190e
Matthes Rieke <m.rieke@52north.org>
2019-06-27 12:08:59 +0200

    fix input max/min occurs

---
+++ src/test/java/org/n52/geoprocessing/wps/client/decoder/stream/DescribeProcessResponseDecoderTest.java
@@ -0,0 +1,72 @@
+/*
+ * ﻿Copyright (C) 2019 52°North Initiative for Geospatial Open Source
+ * Software GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.n52.geoprocessing.wps.client.decoder.stream;
+
+
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+import org.n52.geoprocessing.wps.client.model.InputDescription;
+import org.n52.geoprocessing.wps.client.model.Process;
+
+
+/**
+ *
+ * @author Matthes Rieke <m.rieke@52north.org>
+ */
+public class DescribeProcessResponseDecoderTest {
+
+    @Test
+    public void testInputParsing() throws XMLStreamException {
+        DescribeProcessResponseDecoder decoder = new DescribeProcessResponseDecoder();
+        XMLEventReader xmlReader = XMLInputFactory.newInstance()
+                .createXMLEventReader(new InputStreamReader(this.getClass().getResourceAsStream("/describeprocess-response.xml"),
+                        StandardCharsets.UTF_8));
+        List<Process> result = decoder.readElement(xmlReader);
+
+        Assert.assertThat(result, CoreMatchers.notNullValue());
+        Assert.assertThat(result.size(), CoreMatchers.is(1));
+
+        Process process = result.get(0);
+
+        Assert.assertThat(process.getId(), CoreMatchers.equalTo("de.hsbo.wacodis.snap.s2togeotiff"));
+        Assert.assertThat(process.getInputs().size(), CoreMatchers.is(1));
+        Assert.assertThat(process.getOutputs().size(), CoreMatchers.is(2));
+        
+        InputDescription input1 = process.getInputs().get(0);
+        Assert.assertThat(input1.getId(), CoreMatchers.equalTo("SENTINEL-2_URL"));
+        Assert.assertThat(input1.getMinOccurs(), CoreMatchers.equalTo(1));
+        Assert.assertThat(input1.getMaxOccurs(), CoreMatchers.equalTo(1));
+    }
+    
+    @Test (expected = NumberFormatException.class)
+    public void testInvalidInputParsing() throws XMLStreamException {
+        DescribeProcessResponseDecoder decoder = new DescribeProcessResponseDecoder();
+        XMLEventReader xmlReader = XMLInputFactory.newInstance()
+                .createXMLEventReader(new InputStreamReader(this.getClass().getResourceAsStream("/describeprocess-response_invalid.xml"),
+                        StandardCharsets.UTF_8));
+        List<Process> result = decoder.readElement(xmlReader);
+    }
+
+
+}
